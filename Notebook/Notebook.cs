20aa97ad0594268b1a5d384cb3a28784a1f6db60
mahdi <mahdihasanvandi2540@gmail.com>
2025-06-21 18:13:34 +0330

    Add project files.

---
+++ Notebook/Notebook.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook
+{
+    public class Notebook
+    {
+        List<string> notes = new List<string>();
+
+        /// <summary>
+        /// نمایش لیست دفترچه
+        /// </summary>
+        public void SeeNotebook()
+        {
+            for (int i = 0; i < notes.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{notes[i]}");
+            }
+        }
+
+        /// <summary>
+        /// افزودن یادداشت
+        /// </summary>
+        public void AddNote()
+        {
+            Console.Write("How many notes do you want to add?");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write("Please Enter your note?");
+                string input = Console.ReadLine();
+                notes.Add(input);
+            }
+
+            Console.WriteLine("note added successfully.");
+            Console.WriteLine();
+
+        }
+        /// <summary>
+        /// حذف یادداشت
+        /// </summary>
+        public void RemoveNote()
+        {
+            SeeNotebook();
+            bool deleted = false;
+            while (!deleted)
+            {
+                Console.WriteLine("Are you sure you want to delete this note(1 = yes,2=no)?");
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    Console.Write("which item should be deleted?");
+
+                    int num = int.Parse(Console.ReadLine());
+                    notes.RemoveAt(num - 1);
+                    Console.WriteLine($"deleted item {num}");
+                    deleted = true;
+
+                }
+                else if (input == "2")
+                {
+                    return;
+                    deleted = true;
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter the correct input.");
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ Notebook/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Notebook\n");
+        
+            Console.WriteLine("[1]Add notebook");
+            Console.WriteLine("[2]see notebook");
+            Console.WriteLine("[3]remove notebook");
+            Console.WriteLine("[4]Exit");
+           
+
+            Notebook notebook = new Notebook();
+
+            bool IsValaild = false;
+            while (!IsValaild)
+            {
+                Console.Write("Enter your choice:");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        notebook.AddNote();
+                        break;
+                    case "2":
+                        notebook.SeeNotebook();
+                        break;
+                    case "3":
+                        notebook.RemoveNote();
+                        break;
+                    case "4":
+                       IsValaild=false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter the correct input.");
+                        break;
+
+                }
+            }
+
+
+        }
+
+ 
+    }
+}
