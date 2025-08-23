6e5485619458fcaf9587ac9d2dc204dd76ba8b01
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-08-23 14:28:17 +0330

    project added Calculator and deleted file TaskRira

---
+++ Calculator/Calculator/Numbers.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Calculator
+{   public class Numbers
+    {
+      int number1;
+      int number2;
+      public void GetNumber()
+      {
+            Console.Write("Please Enter number1:");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("Please Enter number2:");
+            number2 = int.Parse(Console.ReadLine());
+      }
+
+
+        public string sum()
+        {
+            string num = $"Sum is:{number1} + {number2} = {number1 + number2}";
+            return num;
+
+        }
+        public string sub()
+        {
+            string num = $"Sub is:{number1} - {number2} = {number1 - number2}";
+            return num;
+        }
+        public string multiple()
+        {
+            string num = $"multiple is:{number1} * {number2} = {number1 * number2}";
+            return num;
+        }
+
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using Calculator;
+
+Numbers numbers = new Numbers();
+numbers.GetNumber();
+
+
+Console.WriteLine("Please Enter Oprator:");
+Console.WriteLine("Sum [+ a A]:");
+Console.WriteLine("Sub [- s S]:");
+Console.WriteLine("Maliple [* m M]");
+string operat = Console.ReadLine();
+
+switch (operat)
+{
+    case "+":
+    case "a":
+    case "A":
+        if(operat == "+" || operat == "a" || operat == "A")
+        Console.WriteLine(numbers.sum());
+        break;
+    case "-":
+    case "s":
+    case "S":
+        if (operat == "-" || operat == "s" || operat == "S")
+        Console.WriteLine(numbers.sub());
+        break;
+    case "*":
+    case "m":
+    case "M":
+        if (operat == "*" || operat == "m" || operat == "M")
+        Console.WriteLine(numbers.multiple());
+        break;
+    default:
+        Console.WriteLine("please Enter the correct phrase!");
+        break;
+}
+Console.ReadLine();
