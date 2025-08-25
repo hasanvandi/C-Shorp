0afcd21374a0a167703bb3b5345b89ea0a3f4a99
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-08-25 15:43:20 +0330

    file guessNumberGame Added

---
+++ GuessNumberGame/PcGuess.cs
@@ -0,0 +1,10 @@
+﻿public class PcGuess
+{
+    Random random = new();
+ 
+    public int randomPc(int num,int num2)
+    {
+       return random.Next(num, num2);
+    }
+
+}
---
+++ GuessNumberGame/Program.cs
@@ -0,0 +1,25 @@
+﻿PcGuess pc = new();
+var rand = pc.randomPc(1, 10);
+int numberGuess = 2;
+for (int i = 0; i < numberGuess; i++)
+{
+    Console.Write($"Please enter guess {i + 1}:");
+    int num = int.Parse(Console.ReadLine());
+    if(num == rand)
+    {
+        Console.WriteLine("verey good you are win!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("try agian!");
+    }
+    if(i == 2)
+    {
+        Console.WriteLine("Oh you are not win...");
+        break;
+    }
+
+}
+
+Console.ReadLine();
