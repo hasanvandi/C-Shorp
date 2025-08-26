2f57be010f69118dd6af9289f8b4f59e1d7417e8
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-08-26 16:55:39 +0330

    check-id project added

---
+++ Check-Id/Check-Id/Person.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Check_Id;
+
+    public class Person
+    {
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public bool Check(int Id)
+    {
+        return ID == Id;
+    }
+}
+
---
+++ Check-Id/Check-Id/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Check_Id
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                people.Add(new Person());
+            }
+
+            Person person = null;
+            
+            for(int i = 0;i < people.Count; i++)
+            {
+                if (people[i].Check(i))
+                {
+                    person = people[i];
+                }
+            }
+        
+            Console.ReadLine();
+        }
+    }
+}
