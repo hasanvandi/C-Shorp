5904b0cfbd87bd67ab39d60eadd96f5f4fa8b18a
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-08-30 16:33:15 +0330

    implici-variable added

---
+++ Implicit-Variable/Implicit-Variable/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello Every body..");
+
+var num01 = 10;
+var num02 = 20;
+var num03 = 30;
+
+var result = (num01 + num02) / num03;
+Console.WriteLine(result);
+
+Console.ReadLine();
+Console.ReadLine();
