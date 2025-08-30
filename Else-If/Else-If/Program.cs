d575dda565b66d07019ed0024b0a204869bae88d
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-08-30 21:02:42 +0330

    else-if added

---
+++ Else-If/Else-If/Program.cs
@@ -0,0 +1,17 @@
+﻿int mahdiAge = 30;
+int rezaAge = 30;
+
+if (mahdiAge > rezaAge)
+{
+    Console.WriteLine("Mahdi is Grater than Reza");
+}
+else if(mahdiAge < rezaAge)
+{
+    Console.WriteLine("Reza is Grater than Mahdi");
+}
+else
+{
+    Console.WriteLine($"age mahdi:{mahdiAge} and age reza:{rezaAge} so \nage mahdi == age reza");
+}
+
+Console.Read();
