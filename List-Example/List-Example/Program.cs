006f759a92f135ce6eb8f445c7262f145bea7262
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-09-02 17:02:26 +0330

    list-example added

---
+++ List-Example/List-Example/Program.cs
@@ -0,0 +1,16 @@
+﻿List<int> ints = new List<int>();
+int sum = 0;
+
+
+do
+{
+    Console.Write($"please enter{ints.Count + 1} element of list:");
+    int number = int.Parse(Console.ReadLine());
+    ints.Add(number);
+    sum += number;
+}
+while (sum < 1000);
+
+Console.WriteLine($"total count of items:{ints.Count}");
+
+Console.ReadLine();
