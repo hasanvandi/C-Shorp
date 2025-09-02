e78f20a9ddff646605f71b3273d60a3af4fb961f
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-09-02 11:51:37 +0330

    while-example added

---
+++ while-example/while-example/Program.cs
@@ -0,0 +1,10 @@
+﻿string userInput = Console.ReadLine();
+while(userInput.Length <= 10)
+{
+    Console.WriteLine("invalid input lenght.please enter more 10 charachter");
+    userInput = Console.ReadLine();
+}
+
+Console.WriteLine("thankyou!");
+
+Console.ReadLine();
